b86cfb02dae440d27ea22d5999d65124ce0a7cd4
Brandon Coffey <bmc939@gmail.com>
2026-08-13 18:13:06 -0500

    update base path for isProd and with /

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: 'VAST-Time-Tracker',
+  basePath: isProd  ? '/VAST-Time-Tracker' : '',
+  assetPrefix: isProd ? '/VAST-Time-Tracker/' : '',
   images: {
     unoptimized: true, // Required for static exports
   },
